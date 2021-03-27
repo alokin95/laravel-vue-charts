c98df5434dc1f51e27d3188c6f6849c75df00992
nikola.mitrovic <nikola.mitrovic@soprex.com>
2021-03-27 15:57:09 +0100

    contract and macaddress entities

---
+++ database/migrations/2021_03_27_144413_create_mac_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMacAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mac_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contract_id')->constrained();
+            $table->string('mac');
+            $table->string('description');
+            $table->string('ssid');
+            $table->boolean('hide_ssid');
+            $table->dateTime('booted_at');
+            $table->string('firmware');
+            $table->string('ip_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mac_addresses');
+    }
+}
